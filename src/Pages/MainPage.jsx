b34c88eda739c550263c02aa08186a360369302b
Axel López Rodríguez <axel.lopez@aumenta.mx>
2022-06-05 17:45:11 -0500

    Header and Footer.

---
+++ src/Pages/MainPage.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+import { Footer } from "../Components/MainPageFooter";
+import { Header } from "../Components/MainPageHeader";
+import { Content } from "../Components/MainPageContent";
+
+const StyledContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: #fff;
+`;
+
+export function MainPage() {
+  return (
+    <StyledContainer>
+      <Header />
+      <Content />
+      <Footer />
+    </StyledContainer>
+  );
+}
